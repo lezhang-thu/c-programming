1afdf0b4a0e14701084285ee7f9d6660a9239ac5
章乐 <lezhang.thu@gmail.com>
2022-10-23 21:09:45 +0800

    tiny updates

--- array/insertion_sort.c
+++ array/insertion_sort.c
@@ -14,9 +14,22 @@ void insertion_sort(int *a, int n) {
     }
 }
 
+void bubble_sort(int *a, int n) {
+    int i, j, t;
+    // n - 1: #times to bubble out
+    for (i = 0; i < n - 1; i++)
+        for (j = 1; j < n - i; j++)
+            if (a[j] < a[j - 1]) {
+                t = a[j];
+                a[j] = a[j - 1];
+                a[j - 1] = t;
+            }
+}
+
 int main(void) {
     int a[] = {5, 2, 4, 6, 1, 3};
-    insertion_sort(a, 6);
+    // insertion_sort(a, 6);
+    bubble_sort(a, 6);
 
     int k;
     for (k = 0; k < 6; k++) printf("%d ", a[k]);
